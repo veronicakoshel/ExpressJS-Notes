648d055009eb66ca4f1a54985fadabf468e7ddcb
veronicakoshel <koshelveronika@mail.ru>
2021-01-10 23:06:57 +0200

    init commit

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://notesdb:Q9a2z699@cluster0-shard-00-00.qqi0w.mongodb.net:27017,cluster0-shard-00-01.qqi0w.mongodb.net:27017,cluster0-shard-00-02.qqi0w.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-keiz0e-shard-0&authSource=admin&retryWrites=true&w=majority'
+};
